8d7815771cd7e036697521c47354cf807a955ef6
Butcher3Years <btech10451.24@bitmesra.ac.in>
2026-02-02 20:25:32 +0530

    About operator overloading !!!! and when to use them!!

---
+++ Operators -- its overloading/Log.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <string>
+
+
+struct Vector2
+{
+     float x, y;
+
+     Vector2(float x,float y)
+            : x(x), y(y) {}
+
+     /*Vector2 Add(const Vector2& other) const  
+      {
+            return *this + other; const ptr in this case so we need to dereference this this keyword!!
+            return operator+(other);
+      
+            } */   
+
+
+
+      Vector2 Add(const Vector2& other) const  
+      {
+            return Vector2(x + other.x, y + other.y);
+      }    
+
+
+      Vector2 operator+(const Vector2& other) const
+       {
+          return Add(other);
+       }
+
+      /*
+      
+             Vector2 operator+(const Vector2& other) const
+           {
+               return Vector2(x + other.x, y + other.y); we have done reverse of the overloading like creating Add from +
+           }
+               */
+       
+
+
+      Vector2 Multiply(const Vector2& other) const
+      {
+            return Vector2(x * other.x, y * other.y );
+      }
+
+      Vector2 operator*(const Vector2& other) const
+      {
+            return Multiply(other);
+      }
+
+};
+
+int main()
+{
+     Vector2 position(4.0f, 4.0f);
+     Vector2 speed(0.5f, 1.5f);
+     Vector2 powerup(1.1f, 1.1f);
+
+
+     Vector2 result1 = position.Add(speed.Multiply(powerup));
+     Vector2 result2 = position + (speed * powerup);//BODMAS
+}
+
